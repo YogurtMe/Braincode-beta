9be6b6a2042273e84cdf2de4f7b8dfc3324d0a86
hyun-netizen <mumarfaruq28@gmail.com>
2025-09-14 10:42:35 +0700

    Add files via upload

---
+++ scripts/dataSoal.js
@@ -0,0 +1,83 @@
+let dataSoal=[
+  {
+    "id": "js-mat-1",
+    "title": "JS Output",
+    "soal": [
+      {
+        "pertanyaan": "apa fungsi console.log pada javascript?",
+        "pilihan": [
+          "Mengambil input",
+          "Melakukan perulangan",
+          "Mengeluarkan output",
+          "Melakukan percabangan"
+        ],
+        "jawaban": 2
+      },
+      {
+        "pertanyaan": "bagaimana penulisan output yang benar pada javascript?",
+        "pilihan": [
+          "log.console = 'Hello, World!'",
+          "console.log = 'Hello, World!'",
+          "output.log('Hello, World!');",
+          "console.log('Hello, World!');"
+        ],
+        "jawaban": 3,
+        "isLast": true
+      }
+    ]
+  },
+  {
+    "id": "py-mat-1",
+    "title": "Definisi Python ",
+    "soal": [
+      {
+        "pertanyaan": "Seperti apa Bahasa pemrograman Python?",
+        "pilihan": [
+          "Pemrograman yang sulit dimengerti",
+          "code program yang sederhana ",
+          "code program yang sulit dipakai",
+          "bahasa pemrograman yang cocok untuk desain "
+        ],
+        "jawaban": 1
+      },
+      {
+        "pertanyaan": "Bahasa Python biasa dipakai pada platform apa?",
+        "pilihan": [
+          "Data science, infrastructure tooling, web, Ai",
+          "membuat mobil, membuat kapal ular",
+          "menjinakkan ular piton",
+          "pembuatan kalkulator sederhana"
+        ],
+        "jawaban": 0,
+        "isLast": true
+      }
+    ]
+  },
+  {
+    "id": "py-mat-2",
+    "title": "Sejarah Python ",
+    "soal": [
+      {
+        "pertanyaan": "Siapa penemu Python?",
+        "pilihan": [
+          "Brendan Rodgers ",
+          "Guido Van De Mosh",
+          "Ahmad Kasim",
+          "Guido Van Rossum"
+        ],
+        "jawaban": 3
+      },
+      {
+        "pertanyaan": "Tahun berapa dirilisnya bahasa pemrograman Python?",
+        "pilihan": [
+          "1991",
+          "1996",
+          "1990",
+          "1989"
+        ],
+        "jawaban": 0,
+        "isLast": true
+      }
+    ]
+  }
+]
